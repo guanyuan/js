22c58764d230a77ff97761662129ed86fef287de
guanyuan <yuanguan89@gmail.com>
2017-10-26 11:19:03 +0800

    实践委托

--- src/bundle.js
+++ src/bundle.js
@@ -1,16 +1,10 @@
 var parent = document.querySelectorAll('.item-list');
 var items = document.querySelectorAll('.item-list .item');
 
-for (var i = 0; i < items.length; i++) {
-  (function(index) {
-    items[index].addEventListener('click', function() {
-      alert(index);
-    })
-  })(i)
-}
-
-
-var newItem = document.createElement('li');
-newItem.innerHTML = '5';
-newItem.className = 'item';
-parent[0].appendChild(newItem)
+parent[0].addEventListener('click', function(e) {
+  for(var i=0; i<items.length; i++) {
+    if(e.target === items[i]) {
+      alert(i);
+    }
+  }
+})
